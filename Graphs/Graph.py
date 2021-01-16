3ad5ff0eb7780834869b0ab2798ed2803a323b61
Bharathi Selvaraj <bharathim@users.noreply.github.com>
2021-01-16 20:40:18 +0530

    Breadth first graph traversal

--- Graphs/Graph.py
+++ Graphs/Graph.py
@@ -1,4 +1,5 @@
 from LinkedList.LinkedList import LinkedList
+from Queues.Queue import Queue
 
 
 class Graph(object):
@@ -9,6 +10,26 @@ class Graph(object):
     def add_edge(self, source, destination):
         if source < self.vertices and destination < self.vertices:
             self.edges[source].prepend(destination)
+
+    def bfs_traversal(self, start):
+        result = []
+
+        queue = Queue()
+        queue.enqueue(start)
+        visited = set()
+
+        while queue:
+            current_vertex = queue.dequeue()
+            result.append(current_vertex)
+            visited.add(current_vertex)
+
+            adjacent_vertex = self.edges[current_vertex].head
+            while adjacent_vertex:
+                if adjacent_vertex.data not in visited:
+                    visited.add(adjacent_vertex.data)
+                    queue.enqueue(adjacent_vertex.data)
+                adjacent_vertex = adjacent_vertex.next
+        return result
 
     def print_graph(self):
         for i in range(self.vertices):
@@ -27,3 +48,4 @@ if __name__ == "__main__":
     g.add_edge(1, 3)
     g.add_edge(2, 3)
     g.print_graph()
+    print(g.bfs_traversal(0))
